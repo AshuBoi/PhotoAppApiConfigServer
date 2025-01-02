fb81e05d5f93c3b68c45e43fd25c3495f4bd1d8a
AshuBoi <ashutosh10jha12@gmail.com>
2025-01-03 04:09:53 +0530

    Initial commit

---
+++ src/main/java/com/ashuboi/photoapp/api/photoappapiconfigserver/PhotoAppApiConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.ashuboi.photoapp.api.photoappapiconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppApiConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppApiConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashuboi/photoapp/api/photoappapiconfigserver/PhotoAppApiConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashuboi.photoapp.api.photoappapiconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
